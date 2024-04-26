343c3f28b29b481d845f7c15f118faee8032662f
NodikoBejanidze <bejanidzenodiko@gmail.com>
2024-04-26 22:14:04 +0400

    second commit

---
+++ day 2/excersise.py
@@ -0,0 +1,3 @@
+gacnoba = "chemi saxelia nodo"
+dabadeba = "02.12.2012"
+print (dabadeba +  "  " + gacnoba)
---
+++ day 3/lesson2.py
@@ -0,0 +1,36 @@
+# print(10+3)
+# print(10-3)
+# print(10 / 2)
+# print(10 * 3)
+# print(10 ** 3)
+# print(10 % 3)
+
+
+
+# num1 = 10
+
+# num2 = 3
+
+# word1 = "mimatebis tamashi: "
+
+# word ="pirvelos damatebuli meore tolia"
+
+# print(num1)
+# print(num2)
+
+# print(num1 + num2)
+
+# print(num1 *num2)
+
+# print(num1 % num2)
+
+a = int(input("tqventvis sasurveli pirveli ricxvi:"))
+b = int(input("tqventvs sasurveli meore ricxvi: "))
+c = int(input("tqventvis sasurveli mesame ricxvi:"))
+
+print(a + b + c)
+print(a - b - c)
+print(a / b / c)
+print(a * b * c)
+print(a ** b ** c)
+print(a % b % c)
